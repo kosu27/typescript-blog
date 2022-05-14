876dc72e6e5972734bda541e2ea6383bbdc60ba1
kosu <kosumon27@gmail.com>
2022-05-14 14:20:34 +0900

    ✨ getStaticPathsの型をつけた

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,7 +3,7 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
-import { NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { Post } from "../../types/post";
 
 const Post: NextPage<{ postData: Post }> = ({ postData }) => {
@@ -23,21 +23,21 @@ const Post: NextPage<{ postData: Post }> = ({ postData }) => {
   );
 };
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths<Pick<Post, "id">> = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
-}
+};
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
-}
+};
 
 export default Post;
